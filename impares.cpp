893bb095fc0ddeb86c0f0a07d822661edc825b29
warrormac <andre.alvarez@ucsp.edu.pe>
2018-09-12 10:15:47 -0500

    Update impares.cpp

--- impares.cpp
+++ impares.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
-    int a,b=1;
+    int a,b=-1;
     cin>>a;
+
     while(b<a)
     {
-        cout<<b;
         b+=2;
-        if (b<a && b>0)
+        cout<<b;
+       if (b<=a && b>=1 && b<a)
             cout<<",";
+
     }
+
 }
